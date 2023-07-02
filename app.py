01244a26a8b5f74f3c8770f85a52457acf3aca2d
garghardik03 <101704069+garghardik03@users.noreply.github.com>
2023-07-02 13:38:31 +0000

    changes

--- app.py
+++ app.py
@@ -1,34 +1,51 @@
 from flask import Flask,render_template, jsonify
-app=Flask(__name__)
+from flask_pymongo import PyMongo
 
-JOBS=[
-  {
-    "id":1,
-    "title":"Data Analyst",
-    "Location":"Bengaluru, India",
-    "Salary":"1000000"
-  },
-  {
-    "id":2,
-    "title":"Data Scientist",
-    "Location":"Delhi, India",
-    "Salary":"1500000"
-  },
-  {
-    "id":3,
-    "title":"Frontend Engineer",
-    "Location":"Remote",
-    "Salary":"1200000"
-  }
-]
+app=Flask(__name__)
+app.config["MONGO_URI"]="mongodb://localhost:27017/Jovian"
+mongo=PyMongo(app)
+# JOBS=[
+#   {
+#     "id":1,
+#     "title":"Data Analyst",
+#     "Location":"Bengaluru, India",
+#     "Salary":"1000000"
+#   },
+#   {
+#     "id":2,
+#     "title":"Data Scientist",
+#     "Location":"Delhi, India",
+#     "Salary":"1500000"
+#   },
+#   {
+#     "id":3,
+#     "title":"Frontend Engineer",
+#     "Location":"Remote",
+#     "Salary":"1200000"
+#   }
+# ]
+
+
 
 @app.route("/")
 def hello_world():
+  jobs=mongo.db.Jobs.find()
+  JOBS=[job for job in jobs]
   return render_template("home.html",jobs=JOBS)
 
 @app.route("/jobs")
 def list_jobs():
+    jobs=mongo.db.Jobs.find()
+    JOBS=[job for job in jobs]
     return jsonify(JOBS)
+
+@app.route("/job/<id>")
+def show_job(id):
+  jobs=mongo.db.Jobs.find()
+  JOBS=[job for job in jobs]
+  job=JOBS[id]
+  return jsonify(job)
+  
   
 if __name__=="__main__":
-  app.run(host="0.0.0.0",debug=True)
+  app.run(host="0.0.0.0",debug=True,port=5001)
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from flask_pymongo import PyMongo
 from flask import Flask
 app=Flask(__name__)
-app.config["MONGO_URI"]="mongodb://localhost:27017/myDatabase"
+app.config["MONGO_URI"]="mongodb://localhost:27017/Jovian"
 mongo=PyMongo(app)
 JOBS=mongo.db.Jobs.find()
 jobs=[job for job in JOBS]
